06828938f66bd6b7eb6fbf6b2d22f75b4732f4b4
babouchka22 <mae.perigot@isen.yncrea.fr>
2023-01-20 22:00:02 +0100

    commit de la parti 4

--- app/src/main/java/fr/isen/perigot/androiderestaurant/DescriptionActivity.kt
+++ app/src/main/java/fr/isen/perigot/androiderestaurant/DescriptionActivity.kt
@@ -2,6 +2,7 @@ package fr.isen.perigot.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
 import fr.isen.perigot.androiderestaurant.databinding.ActivityDescriptionDishBinding
 import fr.isen.perigot.androiderestaurant.databinding.ActivityDetailCategorieBinding
 import fr.isen.perigot.androiderestaurant.modele.Items
@@ -30,10 +31,44 @@ class DescriptionActivity : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar?.title = food
 
-       // binding.dishname.text = dish.nameFr
-       // binding.dishname.text = dishes[position].nameFr
-       // binding.dishIngredient.text = dishes[position].ingredients[0].nameFr
 
+        binding.dishname.text = item.nameFr
+
+        val ingredients = item.ingredients
+        val ingredientString = StringBuilder()
+        ingredients.forEach { ingredient ->
+            ingredientString.append(ingredient.nameFr)
+            ingredientString.append("\n")
+        }
+        binding.dishIngredient.text = ingredientString
+
+
+        if (item.images[0].isNotEmpty()) {
+            Picasso.get()
+                .load(item.images[0])
+                .placeholder(R.drawable.salade_lyonnaise)
+                .into(binding.dishPicture)
+        }
+
+        binding.TotalPrice.text = item.prices.joinToString("\n") { "Total " + it.price + "€" }
+
+        /*
+        binding.boutonMoins.setOnClickListener{
+            if ( dishQuantity == 1)
+            {
+                dishQuantity = 1
+            }
+            else {
+                itemCount -= 1
+            }
+
+        }
+
+        binding.boutonPlus.setOnClickListener {
+            itemCount += 1
+
+        }
+        */
 
     }
 
